85f26f655727aba7f102a2112ab11a6870f52258
Robert Metcalf <rmetcalf9@googlemail.com>
2017-09-23 05:16:00 +0100

    Displaying a simple clock face

--- ProofOfConcepts/test-display-and-control/src/test_display_and_control/core.clj
+++ ProofOfConcepts/test-display-and-control/src/test_display_and_control/core.clj
@@ -27,41 +27,64 @@
   (translate g (first center) (last center))
 
   ;center square
-  (sg/draw g (rect -5 -5 10 10)
+  (sg/draw g (rect -2 -2 2 2)
            (style :foreground (scolor/color :black) :stroke (stroke :width stroke_width))
   )
 
-  ;Draw 4 big ticks
-  (def bigticksize (/ radius 10))
+  ;Draw number marks
+  (def bigticksize (/ radius 5))
+  (def smallticksize (/ radius 20))
+
+  (defn paint-tick [num ticksize] (do
+    (sg/rotate g (* num (/ 360 12)))
+    (sg/draw g (polygon [0 (- 0 radius)] [0 (- 0 (- radius ticksize))])
+        (style :foreground (scolor/color :black) :stroke (stroke :width stroke_width))
+    )
+		;rotate back same amount to reset canvas
+    (sg/rotate g (- 0 (* num (/ 360 12))))
+  ))
 
 	;TRYING to make it work over a list [0 90 180 270]
-  (rotate g 0)
-  (sg/draw g (polygon [0 (- 0 radius)] [0 (- 0 (- radius bigticksize))])
-      (style :foreground (scolor/color :black) :stroke (stroke :width stroke_width))
-  )
-  (rotate g 90)
-  (sg/draw g (polygon [0 (- 0 radius)] [0 (- 0 (- radius bigticksize))])
-      (style :foreground (scolor/color :black) :stroke (stroke :width stroke_width))
-  )
-  (rotate g 180)
-  (sg/draw g (polygon [0 (- 0 radius)] [0 (- 0 (- radius bigticksize))])
-      (style :foreground (scolor/color :black) :stroke (stroke :width stroke_width))
-  )
-  (rotate g 270)
-  (sg/draw g (polygon [0 (- 0 radius)] [0 (- 0 (- radius bigticksize))])
-      (style :foreground (scolor/color :black) :stroke (stroke :width stroke_width))
-  )
+	(paint-tick 1 smallticksize)
+	(paint-tick 2 smallticksize)
+	(paint-tick 3 bigticksize)
+	(paint-tick 4 smallticksize)
+	(paint-tick 5 smallticksize)
+	(paint-tick 6 bigticksize)
+	(paint-tick 7 smallticksize)
+	(paint-tick 8 smallticksize)
+	(paint-tick 9 bigticksize)
+	(paint-tick 10 smallticksize)
+	(paint-tick 11 smallticksize)
+	(paint-tick 12 bigticksize)
 
+
+	(defn paint-hand [angle colour length width] (do
+    (sg/rotate g angle)
+    (sg/draw g (polygon [0 (- 0 length)] [0 0])
+        (style :foreground (scolor/color colour) :stroke (stroke :width width))
+    )
+    (sg/rotate g (- 0 angle))
+	))
+
+	;Draw second hand
+	(paint-hand 10 :red (* 9 (/ radius 10)) (/ stroke_width 2))
+
+	;Draw minute hand
+	(paint-hand 20 :blue (* 7 (/ radius 9)) stroke_width)
+
+	;Draw hour hand
+	(paint-hand 30 :green (* 3(/ radius 6)) (* stroke_width 2))
 
 )
 
 (defn paint-main-canvas [c g]
-  (draw-clock c g [300 300] 50 5)
+  (draw-clock c g [300 300] 150 5)
 )
 
 (def main-canvas
   (sc/canvas :id         :maincanvas
-             :background :blue
+             :background :lightgrey
              :paint paint-main-canvas
   )
 )
