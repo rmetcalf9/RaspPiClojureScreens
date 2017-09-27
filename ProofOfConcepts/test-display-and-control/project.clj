93c45e542e9626be56752a56fc2f1e9acfca73df
Robert Metcalf <rmetcalf9@googlemail.com>
2017-09-27 02:57:31 +0100

    Retrieve initial slack websocket

--- ProofOfConcepts/test-display-and-control/project.clj
+++ ProofOfConcepts/test-display-and-control/project.clj
@@ -8,6 +8,9 @@
     [seesaw "1.4.5"]
     [clj-time "0.14.0"]
     [environ "1.0.2"]
+    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+    [clj-http "2.0.1"]
+    [cheshire "5.5.0"]
   ]
   :main ^:skip-aot test-display-and-control.core
   :target-path "target/%s"
--- ProofOfConcepts/test-display-and-control/src/slack_api/core.clj
+++ ProofOfConcepts/test-display-and-control/src/slack_api/core.clj
@@ -1,12 +1,56 @@
 (ns slack-api.core
   (:gen-class)
-  ;(:require 
-  ;)
+  (:require 
+    [clojure.core.async :as async :refer [go go-loop]]
+    [cheshire.core :refer [parse-string generate-string]]
+    [clj-http.client :as http]
   )
+)
 
+(defn get-websocket-info [api-token]
+  (let [response (-> (http/get "https://slack.com/api/rtm.start"
+                               {:query-params {:token      api-token
+                                               :no_unreads true}
+                                :as :json})
+                     :body)]
+;     (println "Got response for rtm start")
+;     (println (:ok response))
+;     (println (:url response))
+    (when (:ok response)
+      {:botname (:name (:self response)) :url (:url response)}
+    )
+  )
+)
+
+;worker that runs in seperate thread
+(defn worker [{:keys [api-token]}] (do
+  (println "slack worker started")
+  (let [cin (async/chan 10)
+        cout (async/chan 10)
+        {:keys [botname url]} (get-websocket-info api-token)
+        counter (atom 0)
+        next-id (fn []
+                  (swap! counter inc))
+        shutdown (fn []
+                   (async/close! cin)
+                   (async/close! cout))
+        mk-timeout #(async/timeout 15000)]
+    (when (clojure.string/blank? url) (do
+      (println "slack error - nil RTM Wewbsocket URL")
+      (throw (ex-info "Could not get RTM Websocket URL" {}))
+    ))
+
+    (println ":: got websocket url:" url)
+    (println ":: Name of this bot is:" botname)
+  )
+  (println "slack worker ended")
+))
 
 (defn start
   [config]
-  (println "Start Slack API not done")
+  (future (worker config))
+  
+  ;TODO Errors aren't appearing in future. moving out of future to help with debugging
+  ;(worker config)
 )
 
