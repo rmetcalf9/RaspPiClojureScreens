65799fc68555badd1779d22959d566f1cf310a46
Roberts <bdrobert@ic.ac.uk>
2017-12-14 10:27:55 +0000

    update issues #11

--- dolphin/test/dolphin/slack_test.clj
+++ dolphin/test/dolphin/slack_test.clj
@@ -4,7 +4,7 @@
             [dolphin.core :refer :all]))
 
 (deftest register-one-command
-  (testing "Register-one-command to check length of list-of-commands"
+  (testing "Register-one-command to check length of list-of-command"
     (let [length (count @test/list-of-commands)]
       (test/add-to-list-of-commands ["A"])	
       (is (= (+ length 1) (count @test/list-of-commands))))))
